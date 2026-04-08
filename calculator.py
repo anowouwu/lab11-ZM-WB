3af003da714d8337a97e2f28a81db5855872084d
zach m <zachy1590@gmail.com>
2026-04-08 10:58:14 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -6,19 +6,16 @@ One function per operation, in order.
 """
 
 import math
-def add(a, b):
-    return a + b
+def add(a, b): return a + b
 
-def sub(a, b):
-    return a - b
+def sub(a, b): return a - b
 
-def mul(a, b):
-    return a * b
+def mul(a, b): return a * b
 
 def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
+        if a == 0:
+            raise ZeroDivisionError
+        return b / a
 
 def log(a, b):
     try:
@@ -26,19 +23,4 @@ def log(a, b):
     except ValueError as e:
         raise ValueError
 
-def exp(a, b):
-    return a**b
-
-import math
-
-def add(a, b):
-    return a + b
-def subtract(a, b):
-    return a - b
-def multiply(a, b):
-    return a * b
-def logarithm(a, b):
-    try: return math.log(a, b)
-    except ValueError: print("Value error.")
-def exponent(a, b):
-    return a**b
+def exp(a, b): return a**b
